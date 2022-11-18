120edd8f3481a6dafa6bcf978bbb4b22ee4c8a3b
jackiboi12 <98142640+jackiboi12@users.noreply.github.com>
2022-11-18 11:53:32 +0530

    Add files via upload

---
+++ jUnitFunctions.java
@@ -0,0 +1,10 @@
+package jUnitTestPackage;
+
+public class jUnitFunctions {
+	public int addNum(int n1, int n2) {
+		return n1 + n2;
+	}
+	public String addStr(String s1, String s2) {
+		return s1 + s2;
+	}
+}
